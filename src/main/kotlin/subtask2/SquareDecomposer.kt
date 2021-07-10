7e999ed941e23680d190ec0f008de3a628797456
Fileek <83027107+Fileek@users.noreply.github.com>
2021-07-10 19:44:38 +0200

    Complete subtask2

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,33 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number < 5) return null
+
+        val nextNumber = number - 1
+        val sequenceArray = mutableListOf(nextNumber)
+        var array: Array<Int>? = null
+
+        fun composeArray(remainer: Int) {
+            if (remainer == 0) {
+                array = sequenceArray.toTypedArray().reversedArray()
+                return
+            }
+
+            var nx = kotlin.math.sqrt(remainer.toDouble()).toInt()
+
+            while (nx.square() * 2 > remainer) {
+                sequenceArray.add(nx)
+                composeArray(remainer - nx.square())
+                if (array != null) return
+                sequenceArray.remove(nx)
+                nx--
+            }
+        }
+
+        composeArray(number.square() - nextNumber.square())
+        return array
     }
 }
+
+fun Int.square(): Int = this * this
