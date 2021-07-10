c86f8c5d82c6fa0ec1f20a37d4674e4cf3714468
Fileek <83027107+Fileek@users.noreply.github.com>
2021-07-10 19:45:07 +0200

    Complete subtask1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,21 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val posters: Long = array[0].toLong()
+        val colors: Long = array[1].toLong()
+        for (k in 1..colors) {
+            if (colors.factorial() /
+                (k.factorial() * (colors - k).factorial()) == posters) return k.toInt()
+        }
+        return null
     }
+}
+
+fun Long.factorial() : Long {
+    var factorial: Long = 1
+    for (i in 1..this) {
+        factorial *= i
+    }
+    return factorial
 }
